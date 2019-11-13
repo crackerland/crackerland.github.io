a9de6dbac642a3cd45cece0cfc8638296e4d7088
Clay Ackerland <cackerland@usa.wtbts.net>
2019-11-13 16:48:50 -0500

    Added web_modules

---
+++ web_modules/ts-libs.js
@@ -0,0 +1,187 @@
+var Utf8Parser = (function () {
+    function Utf8Parser() {
+    }
+    Utf8Parser.Utf8ArrayToStr = function (array) {
+        var out, i, len, c;
+        var char2, char3;
+        out = "";
+        len = array.length;
+        i = 0;
+        while (i < len) {
+            c = array[i++];
+            switch (c >> 4) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                    out += String.fromCharCode(c);
+                    break;
+                case 12:
+                case 13:
+                    char2 = array[i++];
+                    out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+                    break;
+                case 14:
+                    char2 = array[i++];
+                    char3 = array[i++];
+                    out += String.fromCharCode(((c & 0x0F) << 12) |
+                        ((char2 & 0x3F) << 6) |
+                        ((char3 & 0x3F) << 0));
+                    break;
+            }
+        }
+        return out;
+    };
+    return Utf8Parser;
+}());
+
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var FileLoaderFactory = (function () {
+    function FileLoaderFactory() {
+    }
+    FileLoaderFactory.createFetchHtmlLoader = function (globalFetch) {
+        return new FetchHtmlLoader(globalFetch, "./html/");
+    };
+    return FileLoaderFactory;
+}());
+var FetchHtmlLoader = (function () {
+    function FetchHtmlLoader(globalFetch, root) {
+        this.root = root;
+        this.globalFetch = globalFetch;
+    }
+    FetchHtmlLoader.prototype.load = function (fileName) {
+        return __awaiter(this, void 0, void 0, function () {
+            var filePath, response, stream, _a, done, value;
+            return __generator(this, function (_b) {
+                switch (_b.label) {
+                    case 0:
+                        filePath = this.root + fileName;
+                        return [4, this.globalFetch.fetch(filePath)];
+                    case 1:
+                        response = _b.sent();
+                        if (!response || !response.body) {
+                            return [2, ''];
+                        }
+                        stream = response.body.getReader();
+                        return [4, stream.read()];
+                    case 2:
+                        _a = _b.sent(), done = _a.done, value = _a.value;
+                        return [2, Utf8Parser.Utf8ArrayToStr(value)];
+                }
+            });
+        });
+    };
+    return FetchHtmlLoader;
+}());
+
+var __awaiter$1 = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator$1 = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var View = (function () {
+    function View(viewFile, loader) {
+        this.element = null;
+        this.viewFile = viewFile;
+        this.loader = loader;
+    }
+    View.create = function (viewFile, loader) {
+        return new View(viewFile, loader);
+    };
+    View.prototype.load = function () {
+        return __awaiter$1(this, void 0, void 0, function () {
+            var stringval, view, body;
+            return __generator$1(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (this.element) {
+                            return [2, this.element];
+                        }
+                        return [4, this.loader.load(this.viewFile)];
+                    case 1:
+                        stringval = _a.sent();
+                        view = new DOMParser().parseFromString(stringval, "text/html").firstChild;
+                        if (!view) {
+                            throw new Error("Could not load view from " + this.viewFile);
+                        }
+                        body = view.getElementsByTagName('body');
+                        this.element = body && body[0].firstElementChild ? body[0].firstElementChild : view;
+                        return [2, this.element];
+                }
+            });
+        });
+    };
+    return View;
+}());
+
+export { FileLoaderFactory, View };
